ab6581413ee8a59d55fdb882a3e3c1a7c423835d
fisunoff <fisunoff@yandex.ru>
2023-01-02 20:17:05 +0300

    Расчет углов; пофиксил баг с разным масштабом осей

--- drawer.py
+++ drawer.py
@@ -1,15 +1,18 @@
 import numpy
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
 
 class MyFigure(FigureCanvas):
     """Отрисовка пирамиды и дополнительных построений"""
+
     def __init__(self, width=8, height=4, dpi=100):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.ax = self.fig.add_subplot(projection='3d')
+        self.ax.set_xlabel("x")
+        self.ax.set_ylabel("y")
+        self.ax.set_zlabel("z")
         super(MyFigure, self).__init__(self.fig)  # This sentence is essential, otherwise graphics cannot be displayed
 
     def plot(self, a: list[float], b: list[float], c: list[float], d: list[float]):
@@ -34,7 +37,7 @@ class MyFigure(FigureCanvas):
 
         self.ax.add_collection3d(Poly3DCollection(verts,
                                                   facecolors='cyan', linewidths=1, edgecolors='blue', alpha=.2))
-        plt.show()
+        self.ax.set_aspect('equal')
 
     def draw_vector(self, a: list[float], b: list[float], c: list[float], d: list[float], start: str, finish: str):
         """
@@ -48,11 +51,11 @@ class MyFigure(FigureCanvas):
 
         :return:
         """
-        self.plot(a, b, c, d)
         dots = {"A": a, "B": b, "C": c, "D": d}
         vector = [dots[finish][0] - dots[start][0], dots[finish][1] - dots[start][1], dots[finish][2] - dots[start][2]]
         self.ax.quiver(dots[start][0], dots[start][1], dots[start][2], vector[0], vector[1], vector[2],
                        color="red", linewidths=3)
+        self.ax.set_aspect('equal')
 
     def draw_square(self, a: list[float], b: list[float], c: list[float], d: list[float], vertexes: list[str]):
         """
@@ -65,8 +68,8 @@ class MyFigure(FigureCanvas):
 
         :return:
         """
-        self.plot(a, b, c, d)
         dots = {"A": a, "B": b, "C": c, "D": d}
         vert = [[dots[vertexes[0]], dots[vertexes[1]], dots[vertexes[2]]], ]
         self.ax.add_collection3d(Poly3DCollection(vert,
                                                   facecolors='red', linewidths=2, edgecolors='red', alpha=.5))
+        self.ax.set_aspect('equal')
--- main.py
+++ main.py
@@ -12,16 +12,26 @@ class MyWindow(QtWidgets.QDockWidget):
         self.ui = Ui_DockWidget()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.solve_metrics)
+        self.ui.pushButton_angle.clicked.connect(self.solve_angles)
 
         self.F = MyFigure(width=7, height=7, dpi=100)
         self.ui.horizontalLayout.addWidget(self.F)
 
-    def solve_metrics(self):
-        """В зависимости от ввода находит длину вектора, площадь треугольника или объем пирамиды"""
+    def get_data(self) -> tuple[list, list, list, list]:
+        """
+        Забирает из GUI координаты точек
+
+        :return: Кортеж списков координат точек
+        """
         A = [self.ui.value_Ax.value(), self.ui.value_Ay.value(), self.ui.value_Az.value()]
         B = [self.ui.value_Bx.value(), self.ui.value_By.value(), self.ui.value_Bz.value()]
         C = [self.ui.value_Cx.value(), self.ui.value_Cy.value(), self.ui.value_Cz.value()]
         D = [self.ui.value_Dx.value(), self.ui.value_Dy.value(), self.ui.value_Dz.value()]
+        return A, B, C, D
+
+    def solve_metrics(self):
+        """В зависимости от ввода находит длину вектора, площадь треугольника или объем пирамиды"""
+        A, B, C, D = self.get_data()
         coords = {"A": A, "B": B, "C": C, "D": D}
         line = self.ui.metrics_line.text()
         if re.fullmatch(r"[A-D]{2,4}", line):
@@ -33,6 +43,7 @@ class MyWindow(QtWidgets.QDockWidget):
                                   (coords[p1][2] - coords[p2][2]) ** 2) ** 0.5
                     self.ui.result_name.setText("Длина вектора")
                     self.ui.result_line.setText(f"{vector_len:.9f}")
+                    self.F.plot(A, B, C, D)
                     self.F.draw_vector(A, B, C, D, p1, p2)
                 case 3:  # площадь треугольника
                     p1 = line[0]
@@ -48,6 +59,7 @@ class MyWindow(QtWidgets.QDockWidget):
                     triangle_square = det ** 0.5 / 2
                     self.ui.result_name.setText("Площадь треугольника")
                     self.ui.result_line.setText(f"{triangle_square:.9f}")
+                    self.F.plot(A, B, C, D)
                     self.F.draw_square(A, B, C, D, [p1, p2, p3])
                 case 4:  # ориентированный объем пирамиды
                     p1, p2, p3, p4 = line
@@ -65,6 +77,90 @@ class MyWindow(QtWidgets.QDockWidget):
         else:
             self.ui.result_name.setText("Ошибка")
             self.ui.result_line.setText("Ошибка ввода")
+
+    def solve_angles(self):
+        """Рассчитывает углы между прямыми и плоскостями"""
+        obj1 = self.ui.angle_line_1.text()
+        obj2 = self.ui.angle_line_2.text()
+        if len(obj2) > len(obj1):
+            obj1, obj2 = obj2, obj1  # сначала идет плоскость, потом прямая
+        A, B, C, D = self.get_data()
+        coords = {"A": A, "B": B, "C": C, "D": D}
+        if re.fullmatch(r"[A-D]{2,4}", obj1) and re.fullmatch(r"[A-D]{2,4}", obj2):
+            match [len(obj1), len(obj2)]:
+                case [2, 2]:  # угол между прямыми
+                    vec1 = []
+                    vec2 = []
+                    for i in range(3):
+                        vec1.append(coords[obj1[1]][i] - coords[obj1[0]][i])
+                        vec2.append(coords[obj2[1]][i] - coords[obj2[0]][i])
+                    len1 = (vec1[0]**2 + vec1[1]**2 + vec1[2]**2)**0.5
+                    len2 = (vec2[0]**2 + vec2[1]**2 + vec2[2]**2)**0.5
+                    if len1 and len2:
+                        angle_cos = (vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2]) / (len1 * len2)
+                        angle = numpy.arccos(abs(angle_cos))
+                        self.ui.result_name_angle.setText("Угол между\nпрямыми")
+                        self.ui.result_line_angle_cos.setText(f"acos({angle_cos:.3f})")
+                        self.ui.result_line_angle.setText(f"{angle:.3f} рад.")
+                        self.F.plot(A, B, C, D)
+                        self.F.draw_vector(A, B, C, D, obj1[0], obj1[1])
+                        self.F.draw_vector(A, B, C, D, obj2[0], obj2[1])
+                    else:
+                        self.ui.result_name_angle.setText("Ошибка! Возможно,\nнулевой вектор")
+                        self.ui.result_line_angle.setText("Inf")
+                        self.F.plot(A, B, C, D)
+
+                case [3, 2]:  # угол между плоскостью и прямой
+                    vec11 = []
+                    vec12 = []
+                    vec2 = []
+                    for i in range(3):
+                        vec11.append(coords[obj1[1]][i] - coords[obj1[0]][i])  # 1 вектор плоскости
+                        vec12.append(coords[obj1[2]][i] - coords[obj1[0]][i])  # 2 вектор плоскости
+                        vec2.append(coords[obj2[1]][i] - coords[obj2[0]][i])  # направляющий вектор прямой
+                    n1 = [vec11[1] * vec12[2] - vec11[2]*vec12[1],
+                          vec11[2] * vec12[0] - vec11[0]*vec12[2],
+                          vec11[0] * vec12[1] - vec11[1]*vec12[0]]  # вектор нормали к плоскости
+                    len1 = (n1[0]**2 + n1[1]**2 + n1[2]**2)**0.5
+                    len2 = (vec2[0]**2 + vec2[1]**2 + vec2[2]**2)**0.5
+                    if len1 and len2:
+                        angle_sin = (n1[0] * vec2[0] + n1[1] * vec2[1] + n1[2] * vec2[2]) / (len1 * len2)
+                        angle = numpy.arcsin(abs(angle_sin))
+                        self.ui.result_name_angle.setText("Угол между\nплоскостью\nи прямой")
+                        self.ui.result_line_angle_cos.setText(f"asin({angle_sin:.3f})")
+                        self.ui.result_line_angle.setText(f"{angle:.3f} рад.")
+                        self.F.plot(A, B, C, D)
+                        self.F.draw_square(A, B, C, D, list(obj1))
+                        self.F.draw_vector(A, B, C, D, obj2[0], obj2[1])
+
+                case [3, 3]:  # угол между плоскостями
+                    vec11 = []
+                    vec12 = []
+                    vec21 = []
+                    vec22 = []
+                    for i in range(3):
+                        vec11.append(coords[obj1[1]][i] - coords[obj1[0]][i])  # 1 вектор 1 плоскости
+                        vec12.append(coords[obj1[2]][i] - coords[obj1[0]][i])  # 2 вектор 1 плоскости
+                        vec21.append(coords[obj2[1]][i] - coords[obj2[0]][i])  # 1 вектор 2 плоскости
+                        vec22.append(coords[obj2[2]][i] - coords[obj2[0]][i])  # 2 вектор 2 плоскости
+                    n1 = [vec11[1] * vec12[2] - vec11[2]*vec12[1],
+                          vec11[2] * vec12[0] - vec11[0]*vec12[2],
+                          vec11[0] * vec12[1] - vec11[1]*vec12[0]]  # вектор нормали к 1 плоскости
+                    n2 = [vec21[1] * vec22[2] - vec21[2]*vec22[1],
+                          vec21[2] * vec22[0] - vec21[0]*vec22[2],
+                          vec21[0] * vec22[1] - vec21[1]*vec22[0]]  # вектор нормали ко 2 плоскости
+                    len1 = (n1[0]**2 + n1[1]**2 + n1[2]**2)**0.5
+                    len2 = (n2[0]**2 + n2[1]**2 + n2[2]**2)**0.5
+                    if len1 and len2:
+                        angle_cos = (n1[0] * n2[0] + n1[1] * n2[1] + n1[2] * n2[2]) / (len1 * len2)
+                        angle = numpy.arccos(abs(angle_cos))
+                        self.ui.result_name_angle.setText("Угол между\nплоскостями")
+                        self.ui.result_line_angle_cos.setText(f"acos({angle_cos:.3f})")
+                        self.ui.result_line_angle.setText(f"{angle:.3f} рад.")
+                        self.F.plot(A, B, C, D)
+                        self.F.draw_square(A, B, C, D, list(obj1))
+                        self.F.draw_square(A, B, C, D, list(obj2))
+
 
 
 app = QtWidgets.QApplication([])
