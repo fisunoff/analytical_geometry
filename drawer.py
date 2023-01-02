e12dad2ca4b805c7adc4a6a8b918a9f63e4e1370
fisunoff <fisunoff@yandex.ru>
2023-01-02 13:10:13 +0300

    Вынес в отдельный модуль отрисовку геометрии

---
+++ drawer.py
@@ -0,0 +1,72 @@
+import numpy
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+class MyFigure(FigureCanvas):
+    """Отрисовка пирамиды и дополнительных построений"""
+    def __init__(self, width=8, height=4, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.ax = self.fig.add_subplot(projection='3d')
+        super(MyFigure, self).__init__(self.fig)  # This sentence is essential, otherwise graphics cannot be displayed
+
+    def plot(self, a: list[float], b: list[float], c: list[float], d: list[float]):
+        """
+        Рисует пирамиду
+        :param a: Координаты точки A
+        :param b: Координаты точки B
+        :param c: Координаты точки C
+        :param d: Координаты точки D
+
+        :return:
+        """
+        v = numpy.array([a, b, c, d])
+        self.ax.clear()
+        self.ax.scatter3D(v[:, 0], v[:, 1], v[:, 2])
+
+        verts = [[v[0], v[1], v[2]], [v[0], v[1], v[3]], [v[0], v[2], v[3]], [v[1], v[2], v[3]]]
+        self.ax.text(a[0], a[1], a[2], "a", size=20)
+        self.ax.text(b[0], b[1], b[2], "b", size=20)
+        self.ax.text(c[0], c[1], c[2], "c", size=20)
+        self.ax.text(d[0], d[1], d[2], "d", size=20)
+
+        self.ax.add_collection3d(Poly3DCollection(verts,
+                                                  facecolors='cyan', linewidths=1, edgecolors='blue', alpha=.2))
+        plt.show()
+
+    def draw_vector(self, a: list[float], b: list[float], c: list[float], d: list[float], start: str, finish: str):
+        """
+        Рисует вектор на пирамиде
+        :param a: Координаты точки A
+        :param b: Координаты точки B
+        :param c: Координаты точки C
+        :param d: Координаты точки D
+        :param start: Вершина начала вектора (A, B, C или D)
+        :param finish: Вершина конца вектора (A, B, C или D)
+
+        :return:
+        """
+        self.plot(a, b, c, d)
+        dots = {"A": a, "B": b, "C": c, "D": d}
+        vector = [dots[finish][0] - dots[start][0], dots[finish][1] - dots[start][1], dots[finish][2] - dots[start][2]]
+        self.ax.quiver(dots[start][0], dots[start][1], dots[start][2], vector[0], vector[1], vector[2],
+                       color="red", linewidths=3)
+
+    def draw_square(self, a: list[float], b: list[float], c: list[float], d: list[float], vertexes: list[str]):
+        """
+        Закрашивает на пирамиде грань
+        :param a: Координаты точки A
+        :param b: Координаты точки B
+        :param c: Координаты точки C
+        :param d: Координаты точки D
+        :param vertexes: Наименования вершин, на которых построена грань
+
+        :return:
+        """
+        self.plot(a, b, c, d)
+        dots = {"A": a, "B": b, "C": c, "D": d}
+        vert = [[dots[vertexes[0]], dots[vertexes[1]], dots[vertexes[2]]], ]
+        self.ax.add_collection3d(Poly3DCollection(vert,
+                                                  facecolors='red', linewidths=2, edgecolors='red', alpha=.5))
--- main.py
+++ main.py
@@ -1,52 +1,9 @@
 import numpy
 from PyQt5 import QtWidgets
 import sys
-
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-
+from drawer import MyFigure
 from gui import Ui_DockWidget
 import re
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-
-
-# Create a matplotlib graphics drawing class
-class MyFigure(FigureCanvas):
-    def __init__(self, width=8, height=4, dpi=100):
-        self.fig = Figure(figsize=(width, height), dpi=dpi)
-        self.ax = self.fig.add_subplot(projection='3d')
-        super(MyFigure, self).__init__(self.fig)  # This sentence is essential, otherwise graphics cannot be displayed
-
-    def plot(self, a: list[float], b: list[float], c: list[float], d: list[float]):
-        v = numpy.array([a, b, c, d])
-        self.ax.clear()
-        self.ax.scatter3D(v[:, 0], v[:, 1], v[:, 2])
-
-        verts = [[v[0], v[1], v[2]], [v[0], v[1], v[3]], [v[0], v[2], v[3]], [v[1], v[2], v[3]]]
-        self.ax.text(a[0], a[1], a[2], "a", size=20)
-        self.ax.text(b[0], b[1], b[2], "b", size=20)
-        self.ax.text(c[0], c[1], c[2], "c", size=20)
-        self.ax.text(d[0], d[1], d[2], "d", size=20)
-
-        self.ax.add_collection3d(Poly3DCollection(verts,
-                                                  facecolors='cyan', linewidths=1, edgecolors='blue', alpha=.2))
-        plt.show()
-
-    def draw_vector(self, a: list[float], b: list[float], c: list[float], d: list[float], start: str, finish: str):
-        self.plot(a, b, c, d)
-        dots = {"A": a, "B": b, "C": c, "D": d}
-        vector = [dots[finish][0] - dots[start][0], dots[finish][1] - dots[start][1], dots[finish][2] - dots[start][2]]
-        vector_len = (vector[0] ** 2 + vector[1] ** 2 + vector[2] ** 2) ** 0.5
-        self.ax.quiver(dots[start][0], dots[start][1], dots[start][2], vector[0], vector[1], vector[2], color="red")
-
-    def draw_square(self, a: list[float], b: list[float], c: list[float], d: list[float], vertexes: list[str]):
-        self.plot(a, b, c, d)
-        dots = {"A": a, "B": b, "C": c, "D": d}
-        vert = [[dots[vertexes[0]], dots[vertexes[1]], dots[vertexes[2]]], ]
-        self.ax.add_collection3d(Poly3DCollection(vert,
-                                                  facecolors='red', linewidths=2, edgecolors='red', alpha=.5))
-
 
 
 class MyWindow(QtWidgets.QDockWidget):
@@ -57,7 +14,6 @@ class MyWindow(QtWidgets.QDockWidget):
         self.ui.pushButton.clicked.connect(self.solve_metrics)
 
         self.F = MyFigure(width=5, height=2, dpi=100)
-        # self.F.plot([0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0])
         self.ui.horizontalLayout.addWidget(self.F)
 
     def solve_metrics(self):
@@ -69,7 +25,6 @@ class MyWindow(QtWidgets.QDockWidget):
         coords = {"A": A, "B": B, "C": C, "D": D}
         line = self.ui.metrics_line.text()
         if re.fullmatch(r"[A-D]{2,4}", line):
-            # self.F.plot(A, B, C, D)
             match len(line):
                 case 2:  # длина вектора
                     p1 = line[0]
@@ -94,7 +49,7 @@ class MyWindow(QtWidgets.QDockWidget):
                     self.ui.result_name.setText("Площадь треугольника")
                     self.ui.result_line.setText(f"{triangle_square:.9f}")
                     self.F.draw_square(A, B, C, D, [p1, p2, p3])
-                case 4:
+                case 4:  # ориентированный объем пирамиды
                     p1, p2, p3, p4 = line
                     vec12 = (coords[p2][0] - coords[p1][0], coords[p2][1] - coords[p1][1],
                              coords[p2][2] - coords[p1][2])  # вектор из 1 точки во 2
